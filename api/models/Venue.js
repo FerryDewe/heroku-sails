9a23910900e12ff963841be57ff8d489f74150b8
FerryDewe <ferry@solusi.mu>
2014-09-04 19:13:32 +0700

    Test Heroku
    Tambah Model dan H1 di Homepage ejs

---
+++ api/models/Venue.js
@@ -0,0 +1,39 @@
+/**
+* Venue.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	nama: {
+            type: 'STRING',
+            required: true
+        },
+  	namaRuangan: 'string',
+  	jalan: 'string',
+  	alamatTambahan: 'string',
+  	kota: 'string',
+    provinsi: 'string',
+  	negara: 'string',
+  	kodePos: 'string',
+  	latitude: 'string',
+  	longitude: 'string',
+  	webURL: 'string',
+    kapasitas: 'int',
+    placeID: 'string',
+    tipe: {
+      type: 'string'
+    },
+    events:{
+    	collection: 'event',
+    	via: 'tempat'
+    }
+
+  }
+
+
+};
+
